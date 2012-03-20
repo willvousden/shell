2cd2773d47c06459232a600e2e0e58b37ccac134
Will Vousden <will@vousden.me.uk>
2012-03-20 18:47:00 +0000

    Improved line-endings.sh.

--- bashrc.d/line-endings.sh
+++ bashrc.d/line-endings.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
 if [[ ! -x $(which unix2dos) ]] && [[ -x $(which perl) ]]; then
-	function dos2unix {
-		if [[ -r $1 ]] && [[ -w $2 ]]; then
-			perl -p -e 's/\r$//' < $1 > $2
-		fi
-	}
+    function dos2unix {
+        __patternify 's/\r$//' $1 $2
+    }
 fi
 
 if [[ ! -x $(which unix2dos) ]] && [[ -x $(which perl) ]]; then
-	function unix2dos {
-		if [[ -r $1 ]] && [[ -w $2 ]]; then
-			perl -p -e 's/\n/\r\n/' < $1 > $2
-		fi
-	}
+    function unix2dos {
+        __patternify 's/\n/\r\n/' $1 $2
+    }
 fi
+
+function __patternify {
+    local source=$2
+    local destination=$3
+    if [[ -z $destination ]]; then
+        destination=$(mktemp $2.XXXX)
+    fi
+    echo $source $destination
+
+    if [[ -r $source ]] && [[ -w $destination ]]; then
+        perl -p -e $1 < $source > $destination
+    fi
+
+    if [[ -z $3 ]]; then
+        mv $destination $source
+    fi
+}
