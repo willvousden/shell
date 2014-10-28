f29212ba529e3768140bc4be0d7f38df2eb0755c
Will Vousden <will@vousden.me.uk>
2014-10-28 12:45:53 +0000

    Spruced up python-startup.py.

--- profile.d/python-startup.py
+++ profile.d/python-startup.py
@@ -1,3 +1,18 @@
 import rlcompleter
 import readline
 readline.parse_and_bind("tab: complete")
+
+# Try to import useful modules but fail silently if they aren't there.
+try:
+    import numpy as np
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+
+    default = mpl.get_backend()
+    target = 'TkAgg'
+    try:
+        plt.switch_backend(target)
+    except ImportError:
+        plt.switch_backend(default)
+except ImportError:
+    pass
