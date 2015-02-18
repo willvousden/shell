545ba7385b8749253a31d064caeb85aac83b9543
Will Vousden <will@vousden.me.uk>
2015-02-18 11:43:11 +0000

    Updated ligo-proxy stuff.

--- bashrc.d/ligo-proxy.sh
+++ bashrc.d/ligo-proxy.sh
@@ -2,12 +2,25 @@
 
 export LIGO_USER=will.vousden
 
+ligo-proxy-test() {
+    local proxy_info=$(grid-proxy-info 2> /dev/null)
+    if [[ -z $proxy_info ]] || [[ $proxy_info =~ timeleft\ *:\ *0:00:00 ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
 ligo-proxy() {
     alias ssh=gsissh
     alias scp=gsiscp
     export GIT_SSH=gsissh
-    local proxy_info=$(grid-proxy-info 2> /dev/null)
-    if [[ -z $proxy_info ]] || [[ $proxy_info =~ timeleft\ *:\ *0:00:00 ]]; then
+    if ! ligo-proxy-test; then
         ligo-proxy-init $LIGO_USER
     fi
 }
+
+# If there's one available, set up aliases, etc.
+if ligo-proxy-test; then
+    ligo-proxy
+fi
