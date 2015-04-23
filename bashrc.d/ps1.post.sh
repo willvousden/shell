2a880f11da74af74123d18646b848a422ab1f9da
Will Vousden <will@vousden.me.uk>
2015-04-23 12:57:56 +0100

    Cleaned up PS1 stuff (removed SVN).

--- bashrc.d/ps1.post.sh
+++ bashrc.d/ps1.post.sh
@@ -1,11 +1,8 @@
-# Generate dirty status flag for Git and SVN.
-__git_svn_status () {
+# Generate dirty status flag for Git.
+__git_status () {
     if [[ $(type -t git) ]] && git status --porcelain 2> /dev/null | grep -q '^\s*[ACDMRU]'; then
 		# Generate Git output for PS1.
         echo -n $1
-    elif [[ $(type -t svn) ]] && svn info &> /dev/null; then
-		# Generate SVN output for PS1.
-		svn status | grep -q '^\s*[?ACDMR?!]' && echo -n $1
 	fi
 }
 
@@ -17,35 +14,18 @@ __git_added_flag () {
     git status --porcelain 2> /dev/null | grep -q '^\s*??' && echo -n $1
 }
 
-# Generate branch/location information for Git and SVN repositories.
-__git_svn_tag () {
+# Generate branch/location information for Git repositories.
+__git_tag () {
 	local tag=
     if [[ $(type -t git) ]] && [[ $(type -t __git_ps1) ]]; then
         # Generate Git output using standard completion function.
         tag=$(__git_ps1 '%s')
     elif [[ $(type -t git) ]] && git status --porcelain &> /dev/null; then
         tag=?
-    elif [[ $(type -t svn) ]] && svn info 2> /dev/null; then
-		# Generate SVN output for PS1.
-		local url=`svn info | awk '/URL:/ {print $2}'`
-		if [[ $url =~ trunk ]]; then
-			tag=trunk
-		elif [[ $url =~ /branches/ ]]; then
-			tag=$(echo -n $url | sed -e 's#^.*/\(branches/.*\)/.*$#\1#')
-		elif [[ $url =~ /tags/ ]]; then
-			tag=$(echo -n $url | sed -e 's#^.*/\(tags/.*\)/.*$#\1#')
-		else
-			tag=..
-		fi
-	fi
-
-	format=' (%s)'
-	if [[ $1 ]]; then
-		format=$1
 	fi
 
 	if [[ $tag ]]; then
-		printf "$format" $tag
+		printf "${1:- (%s)}" $tag
 	fi
 }
 
@@ -71,16 +51,15 @@ __prompt_command() {
     # Set a standard PS1 contents: user@host:dir (with colours).
     local ps1_inner="$(c $user_color)\u@\h$(c $off):$(c $blue)\W$(c $off)"
 
-    # If "better PS1" is asked for, augment this with (coloured) Git/SVN
-    # information.
+    # If "better PS1" is asked for, augment this with (coloured) Git information.
     if [[ $BETTER_PS1 == true ]]; then
         local dirty_flag='*'
         local stash_flag='+'
         local added_flag='?'
 
-        ps1_inner+='$(__git_svn_tag "|'$(c $yellow)'%s")'
+        ps1_inner+='$(__git_tag "|'$(c $yellow)'%s")'
         ps1_inner+='$(__git_stash_flag "'$(c $yellow)$stash_flag'")'
-        ps1_inner+='$(__git_svn_status "'$(c $red)$dirty_flag'")'
+        ps1_inner+='$(__git_status "'$(c $red)$dirty_flag'")'
         ps1_inner+='$(__git_added_flag "'$(c $purple)$added_flag'")'$(c $off)
     fi
 
