88e186d8cd2bad7ee95940c40ad6d9107d940289
Will Vousden <will@vousden.me.uk>
2015-06-06 22:11:38 +0100

    Updates to ssht.

--- bashrc.d/ssh-tmux.sh
+++ bashrc.d/ssh-tmux.sh
@@ -1,22 +1,25 @@
 ssht() {
+    local detach=
+    if [[ $1 == '-d' ]]; then
+        detach='-d'
+        shift
+    fi
+
     if [[ -z $1 ]]; then
         echo "No hostname."
         return 1
     fi
 
     if [[ $2 ]]; then
-        command="tmux a -t $2 || tmux new -s $2"
+        local tmux_command="tmux a $detach -t $2 || tmux new -s $2"
     else
-        command="tmux a || tmux new"
+        local tmux_command="tmux a $detach || tmux new"
     fi
-
-    if [[ $1 == -d ]]; then
-        command="tmux detach -a 2> /dev/null; $command"
-    fi
-
+    
     local ssh_command=ssh
     if [[ $(type -t ssh) == alias ]]; then
         ssh_command=$(alias ssh | sed -Ee "s/^alias ssh='(.+)'$/\1/")
     fi
-    eval $ssh_command -Xt $1 \"$command\"
+    local command="$ssh_command -Xt $1 \"$tmux_command\""
+    eval $command
 }
