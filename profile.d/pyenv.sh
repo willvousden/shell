6a81d67cdf063a9eb4d05bcff29ea0df4909e78d
Will Vousden <will@vousden.me.uk>
2015-06-16 23:16:11 +0100

    Fixed pyenv stuff.

--- profile.d/pyenv.sh
+++ profile.d/pyenv.sh
@@ -2,8 +2,20 @@ if [[ -z $PYENV_ROOT ]] && [[ -d $HOME/.pyenv ]]; then
     export PYENV_ROOT=$HOME/.pyenv
 fi
 
-if [[ -n $PYENV_ROOT ]] && [[ -x $PYENV_ROOT/bin/pyenv ]]; then
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-    #eval "$(pyenv virtualenv-init -)"
+if [[ -n $PYENV_ROOT ]]; then
+    ! which pyenc 2> /dev/null > /dev/null
+    found=$?
+    if [[ $found != 0 ]]; then
+        # Wasn't in path.
+        if [[ -x $PYENV_ROOT/bin/pyenv ]]; then
+            # But we've found it anyway.
+            export PATH="$PYENV_ROOT/bin:$PATH"
+            found=0
+        fi
+    fi
+
+    if [[ $found == 0 ]]; then
+        eval "$(pyenv init -)"
+        #eval "$(pyenv virtualenv-init -)"
+    fi
 fi
