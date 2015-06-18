8eaf42258bffccb24f82f131d9a9c9f670aeb9e4
Will Vousden <will@vousden.me.uk>
2015-06-18 10:06:08 +0100

    Added rudimentary completion for qstat and qdel.

---
+++ bashrc.d/pbs-completion.sh
@@ -0,0 +1,11 @@
+if type qdel 2> /dev/null > /dev/null && type qstat 2> /dev/null > /dev/null; then
+    _pbs_jobs()
+    {
+        local word=${COMP_WORDS[COMP_CWORD]}
+        local ids=$(qstat -u $USER | tail -n +6 | awk '{print $1}')
+        COMPREPLY=($(compgen -W "$ids" -- $word))
+    }
+
+    complete -F _pbs_jobs qstat
+    complete -F _pbs_jobs qdel
+fi
