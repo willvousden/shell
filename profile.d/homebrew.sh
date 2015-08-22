8be6e22f49b2ea77c0a01bc8f21cd80b0df1cbab
Will Vousden <will@vousden.me.uk>
2015-08-22 13:11:23 +0100

    Added Homebrew sbin to PATH.

--- profile.d/homebrew.sh
+++ profile.d/homebrew.sh
@@ -3,6 +3,7 @@ if [[ -x /usr/local/bin/brew ]]; then
     # ...then set the relevant paths.
     prefix=/usr/local
     export PATH="$prefix/bin:$PATH"
+    export PATH="$prefix/sbin:$PATH"
     export MANPATH="$prefix/share/man:$MANPATH"
     if [[ -d $prefix/opt/coreutils ]]; then
         export PATH="$prefix/opt/coreutils/libexec/gnubin:$PATH"
