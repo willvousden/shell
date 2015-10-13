951d53356b9dd5cb38cbd3bd2563c289ce2996be
Will Vousden <will@vousden.me.uk>
2015-10-13 11:50:44 +0100

    Updated Python startup script.

--- profile.d/python-startup.py
+++ profile.d/python-startup.py
@@ -7,16 +7,27 @@ readline.parse_and_bind("tab: complete")
 # Try to import useful modules but fail silently if they aren't there.
 try:
     import numpy as np
-    import matplotlib as mpl
-    import matplotlib.pyplot as plt
-
-    default = mpl.get_backend()
-    target = 'TkAgg'
     try:
-        plt.switch_backend(target)
-        f = plt.figure()
-        del f
-    except:
-        plt.switch_backend(default)
+        import pandas as pd
+    except ImportError:
+        pass
+
+    try:
+        import matplotlib as mpl
+        import matplotlib.pyplot as pp
+
+        # Try to activate TkAgg backend, but roll back on failure.
+        default = mpl.get_backend()
+        target = 'TkAgg'
+        try:
+            pp.switch_backend(target)
+            f = pp.figure()
+            del f
+        except:
+            pp.switch_backend(default)
+        finally:
+            del target, default
+    except ImportError:
+        pass
 except ImportError:
     pass
