2c51b98f2e221e493a7064acec561c76f557bdf3
Will Vousden <will@vousden.me.uk>
2016-07-19 13:45:16 +0100

    Removed some old rc files.

--- bashrc.d/0-globus.sh
+++
@@ -1,7 +0,0 @@
-# If expected environment variables aren't set, run set-up script.
-if [[ -z $GLOBUS_PATH ]] && [[ -d /opt/ldg ]]; then
-    export GLOBUS_LOCATION=/opt/ldg
-    if [[ -f $GLOBUS_LOCATION/etc/globus-user-env.sh ]] ; then
-        . $GLOBUS_LOCATION/etc/globus-user-env.sh
-    fi
-fi
--- bashrc.d/0-ssh-clipper.sh
+++
@@ -1,4 +0,0 @@
-# export CLIPPER_PORT=8378
-
-# alias sshc="rm -f ~/.ssh/cm_socket/* 2> /dev/null"
-# alias ssh="ssh -R localhost:$CLIPPER_PORT:localhost:$CLIPPER_PORT"
