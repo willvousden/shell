d77b611abc2f70da331beaf49433bbfc9476df32
Will Vousden <will@vousden.me.uk>
2016-12-02 09:50:41 +0000

    Fixed ssh-agent.

--- bash_profile.d/ssh-agent.sh
+++ bash_profile.d/ssh-agent.sh
@@ -1,20 +1,26 @@
-SSH_AGENT_FILE=$HOME/.ssh-agent/$(hostname -s)
-SSH_AGENT_CONTENT=
+export SSH_AGENT_FILE=$HOME/.ssh-agent/$(hostname -s)
+export SSH_AGENT_CONTENT=
 [[ -d $HOME/.ssh-agent ]] || mkdir $HOME/.ssh-agent
 if [[ ! -f $SSH_AGENT_FILE ]]; then
     # The file doesn't exist, so kill any existing ssh-agent and start a new one,
     # creating a new file.
     killall ssh-agent 2> /dev/null
     SSH_AGENT_CONTENT=$(ssh-agent)
-elif [[ ! $(ps -u $USER | grep ssh-agent | grep -v grep) ]]; then
-    # The file does exist, but there's no process, so start a new one and create a new file.
-    SSH_AGENT_CONTENT=$(ssh-agent)
-fi
-if [[ $SSH_AGENT_CONTENT ]]; then
-    echo -n "New ssh-agent: "
-    eval $(echo "$SSH_AGENT_CONTENT" | grep echo)
-    echo "$SSH_AGENT_CONTENT" > $SSH_AGENT_FILE
+    eval $SSH_AGENT_CONTENT
+else
+    # Evaluate the file contents.
+    SSH_AGENT_CONTENT=$(<$SSH_AGENT_FILE)
+    eval $(<<<"$SSH_AGENT_CONTENT" grep -v echo)
+
+    # Is there an existing ssh-agent with the specified PID?
+    read _comm _user <<<"$(ps -p "$SSH_AGENT_PID" -o comm= -o user=)"
+    if [[ $_comm != ssh-agent || $_user != $USER ]]; then
+        # No, so create a new one.
+        killall ssh-agent 2> /dev/null
+        SSH_AGENT_CONTENT=$(ssh-agent)
+        eval $SSH_AGENT_CONTENT
+    fi
 fi
 
-# Evaluate the file contents.
-eval $(cat $SSH_AGENT_FILE | grep -v echo)
+# Update this host's ssh-agent file.
+echo "$SSH_AGENT_CONTENT" > $SSH_AGENT_FILE
