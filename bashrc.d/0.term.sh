55eb8b41b486266cc22aa5beac2149d6c9466f90
Will Vousden <will@vousden.me.uk>
2016-12-25 20:00:25 +0000

    Fixed TERM for OS X.

--- bashrc.d/0.term.sh
+++ bashrc.d/0.term.sh
@@ -1,8 +1,12 @@
 # Set term type.
-if [[ $COLORTERM = "gnome-terminal" ]] || \
-   [[ $COLORTERM = "xfce4-terminal" ]] || \
-   [[ $COLORTERM = "mate-terminal" ]]; then
-    export TERM=xterm-256color
-elif [[ $COLORTERM = "rxvt-xpm" ]]; then
-    export TERM=rxvt-256color
+if [[ $TERM == screen ]]; then
+    # Ham-fisted fix.
+    TERM=screen-256color
 fi
+# if [[ $COLORTERM = "gnome-terminal" ]] || \
+   # [[ $COLORTERM = "xfce4-terminal" ]] || \
+   # [[ $COLORTERM = "mate-terminal" ]]; then
+    # export TERM=xterm-256color
+# elif [[ $COLORTERM = "rxvt-xpm" ]]; then
+    # export TERM=rxvt-256color
+# fi
