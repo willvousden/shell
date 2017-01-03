b123dd6e01874fc40261a868b1f583bd11170eb8
Will Vousden <will@vousden.me.uk>
2017-01-03 09:08:39 +0000

    Little fixes.

--- bash_profile.d/ssh-agent.sh
+++ bash_profile.d/ssh-agent.sh
@@ -6,11 +6,11 @@ if [[ ! -f $SSH_AGENT_FILE ]]; then
     # creating a new file.
     killall ssh-agent 2> /dev/null
     SSH_AGENT_CONTENT=$(ssh-agent)
-    eval $SSH_AGENT_CONTENT
+    eval "$SSH_AGENT_CONTENT"
 else
     # Evaluate the file contents.
     SSH_AGENT_CONTENT=$(<$SSH_AGENT_FILE)
-    eval $(<<<"$SSH_AGENT_CONTENT" grep -v echo)
+    eval "$(<<<"$SSH_AGENT_CONTENT" grep -v echo)"
 
     # Is there an existing ssh-agent with the specified PID?
     read _comm _user <<<"$(ps -p "$SSH_AGENT_PID" -o comm= -o user=)"
@@ -18,7 +18,7 @@ else
         # No, so create a new one.
         killall ssh-agent 2> /dev/null
         SSH_AGENT_CONTENT=$(ssh-agent)
-        eval $SSH_AGENT_CONTENT
+        eval "$SSH_AGENT_CONTENT"
     fi
 fi
 
