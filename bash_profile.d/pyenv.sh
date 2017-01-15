d1c522ff39b11cd6824e8dd47ce69c7ad014f342
Will Vousden <will@vousden.me.uk>
2017-01-15 10:54:46 +0000

    Corrected pyenv behaviour.

--- bash_profile.d/pyenv.sh
+++ bash_profile.d/pyenv.sh
@@ -3,19 +3,11 @@ if [[ -z $PYENV_ROOT ]] && [[ -d $HOME/.pyenv ]]; then
 fi
 
 if [[ -n $PYENV_ROOT ]]; then
-    hash pyenv 2> /dev/null
-    found=$?
-    if [[ $found != 0 ]]; then
+    if ! hash pyenv 2> /dev/null; then
         # Wasn't in path.
         if [[ -x $PYENV_ROOT/bin/pyenv ]]; then
             # But we've found it anyway.
             export PATH="$PYENV_ROOT/bin:$PATH"
-            found=0
         fi
-    fi
-
-    if [[ $found == 0 ]]; then
-        eval "$(pyenv init -)"
-        #eval "$(pyenv virtualenv-init -)"
     fi
 fi
