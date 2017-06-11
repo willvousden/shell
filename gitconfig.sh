41ea21cb7662fce4f5b20377ad6a53861fe475e1
Will Vousden <will@vousden.me.uk>
2017-06-11 20:59:09 +0100

    Removed verlt from deployment scripts.

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
 
-verlte() {
-    [ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
-}
-
-verlt() {
-    [ "$1" = "$2" ] && return 1 || verlte $1 $2
-}
-
 git_version=$(git --version)
 git_version=${git_version#'git version'}
 
-if verlt "$git_version" 1.7.11; then
+if vercomp "$git_version" '<' 1.7.11; then
     git config --global push.default nothing
 else
     git config --global push.default simple
 fi
 
-if verlt "$git_version" 1.7.2; then
+if vercomp "$git_version" '<' 1.7.2; then
     git config --global alias.s "status --short"
 else
     git config --global alias.s "status --short --branch"
