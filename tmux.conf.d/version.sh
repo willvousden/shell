0f3ef5510dc46ab9a74a584ef4837990de46a9fd
Will Vousden <will@vousden.me.uk>
2017-06-11 20:54:23 +0100

    Rewrote versioning for tmux altogether.

---
+++ tmux.conf.d/version.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if vercomp "$TMUX_VERSION" '<' 2.1; then
+    tmux source-file ~/.tmux.conf.d/old
+elif vercomp "$TMUX_VERSION" '>=' 2.1 && vercomp "$TMUX_VERSION" '<' 2.4; then
+    tmux source-file ~/.tmux.conf.d/2.1
+elif vercomp "$TMUX_VERSION" '>' 2.4; then
+    tmux source-file ~/.tmux.conf.d/2.4
+fi
