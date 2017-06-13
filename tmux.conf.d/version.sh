f29fdb162468df4a7373affcd6d376068e911f24
Will Vousden <will@vousden.me.uk>
2017-06-13 11:46:28 +0100

    Fixed tmux version typo.

--- tmux.conf.d/version.sh
+++ tmux.conf.d/version.sh
@@ -3,6 +3,6 @@ if vercomp "$TMUX_VERSION" '<' 2.1; then
     tmux source-file ~/.tmux.conf.d/old
 elif vercomp "$TMUX_VERSION" '>=' 2.1 && vercomp "$TMUX_VERSION" '<' 2.4; then
     tmux source-file ~/.tmux.conf.d/2.1
-elif vercomp "$TMUX_VERSION" '>' 2.4; then
+elif vercomp "$TMUX_VERSION" '>=' 2.4; then
     tmux source-file ~/.tmux.conf.d/2.4
 fi
