7a5d138c3335018471c5bc9d3d0133d0a1c9193f
Will Vousden <will@vousden.me.uk>
2018-01-12 21:27:44 +0000

    Fixed matplotlib.sh for python3 compatibility.

--- matplotlib.sh
+++ matplotlib.sh
@@ -1,10 +1,19 @@
 colours=(456BA3 CA8000 7B9F16 D4400F 7161A1 B15700 4086B4 E6AC00 954A8B 838700 D23211 4769C3 DF8800 A94169 2CA455)
 
-version=$(python -c 'import matplotlib; print(matplotlib.__version__)' 2> /dev/null)
-if [[ $? == 0 ]]; then
-    if vercomp "$version" '<' 1.5; then
-        list=$(printf '%s, ' "${colours[@]}" | sed 's/, $//')
-        cat <<FOO
+# Prefer Python 3.
+python=
+if hash python3 2> /dev/null; then
+    python=python3
+elif hash python 2> /dev/null; then
+    python=python
+fi
+
+if [[ $python ]]; then
+    version=$("$python" -c 'import matplotlib; print(matplotlib.__version__)' 2> /dev/null)
+    if [[ $? == 0 ]]; then
+        if vercomp "$version" '<' 1.5; then
+            list=$(printf '%s, ' "${colours[@]}" | sed 's/, $//')
+            cat <<FOO
 axes.color_cycle    : $list
 FOO
     else
@@ -12,5 +21,6 @@ FOO
         cat <<FOO
 axes.prop_cycle    : cycler('color', [$list])
 FOO
+        fi
     fi
 fi
