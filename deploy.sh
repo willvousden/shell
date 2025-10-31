ca95c5758563c8f28ce0e88f7de537b7ce006aa7
Will Vousden <152388+willvousden@users.noreply.github.com>
2025-10-31 08:32:04 +0100

    Refactored deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,54 +2,47 @@
 
 set -euo pipefail
 
+
+# Symlink to files only; create directories as necessary.
+symlink_tree() {
+    local source_prefix
+    local target_prefix
+    local mode
+    source_prefix=${1%/}
+    target_prefix=${2}
+    mode=${3-ln}
+
+    shopt -s globstar
+    for item in "$source_prefix"/**/*; do
+        if [[ -f $item ]]; then
+            relative_path=${item#"$source_prefix/"}
+            target_path="${target_prefix}${relative_path}"
+            mkdir -p "$(dirname "$target_path")"
+            if [[ $mode == cp ]]; then
+                echo cp "$item" "$target_path"
+                cp "$item" "$target_path"
+            else
+                ln -snfv "$(readlink -f "$item")" "$target_path"
+            fi
+        fi
+    done
+}
+
+
 DIRCOLORS=solarized.ansi-dark
 
-install_dotfiles()
-{
-    # Symlink to files only; create directories as necessary.
-    shopt -s globstar
-    for item in dotfiles/**/*; do
-        target_path=~/."${item#dotfiles/}"
-        mkdir -p "$(dirname "$target_path")"
-        if [[ -f $item ]]; then
-            ln -snfv "$(readlink -f "$item")" "$target_path"
-        fi
-    done
-
-    ln -svf ~/.dircolors.d/"${DIRCOLORS}" ~/.dir_colors
-}
-
-install_ssh()
-{
-    [[ -d ~/.ssh ]] || mkdir ~/.ssh
-    [[ -d ~/.ssh/cm_socket ]] || mkdir -p ~/.ssh/cm_socket
-    cp "$(pwd)/ssh/authorized_keys" ~/.ssh/authorized_keys
-    cp "$(pwd)/ssh/config" ~/.ssh/config
-    [[ ! -f ~/.ssh/config.local ]] || cat ~/.ssh/config.local >> ~/.ssh/config
-    chmod -R og-rxw ~/.ssh
-}
-
-install_ipython_profile()
-{
-    if ! hash ipython &> /dev/null; then
-        return
-    fi
-
-    local profile_dir
-    profile_dir=$(ipython locate profile)
-
-    shopt -s globstar
-    for item in ipython/**/*; do
-        target_path="$profile_dir/${item#ipython/}"
-        mkdir -p "$(dirname "$target_path")"
-        if [[ -f $item ]]; then
-            ln -snfv "$(readlink -f "$item")" "$target_path"
-        fi
-    done
-}
-
-install_dotfiles
-install_ssh
-install_ipython_profile
-
+# Install regular dotfiles.
+symlink_tree dotfiles/ ~/.
+ln -svf ~/.dircolors.d/"${DIRCOLORS}" ~/.dir_colors
 ln -snfv "$(readlink -f bin)" ~/.bin
+
+# Install IPython stuff.
+if command -v ipython &> /dev/null; then
+    symlink_tree ipython/ "$(ipython locate profile)/"
+fi
+
+# Install SSH config.
+symlink_tree ssh/ ~/.ssh/ cp
+[[ -d ~/.ssh/cm_socket ]] || mkdir -p ~/.ssh/cm_socket
+[[ ! -f ~/.ssh/config.local ]] || cat ~/.ssh/config.local >> ~/.ssh/config
+chmod -R og-rxw ~/.ssh
