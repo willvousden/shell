02f0a59c2bc408b87f6239e3663cc9c1df4169c3
Will Vousden <152388+willvousden@users.noreply.github.com>
2025-11-03 20:44:06 +0100

    Removed some old files

--- dotfiles/bashrc.d/5.pyenv.sh
+++
@@ -1,6 +0,0 @@
-# If pyenv exists as an executable file, but isn't yet a shell function...
-if hash pyenv 2> /dev/null && [[ $(type -t pyenv) != function ]]; then
-    # ... then initialise pyenv.
-    eval "$(pyenv init -)"
-    #eval "$(pyenv virtualenv-init -)"
-fi
